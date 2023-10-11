a371b12ed3acefa28ea29d44813b6fb07a9ebbdf
inzilion <inzilion@gmail.com>
2023-10-11 12:50:20 +0900

    네브바 완성?

--- src/components/authButton.js
+++ src/components/authButton.js
@@ -17,13 +17,13 @@ export default function AuthButton(){
     },
     {
       name: "Setting",
-      href: "/proflie/setting",
+      href: "/profile/setting",
       isMobile: isMobile,
       handler: null,
     },
     {
-      name: "Create",
-      href: "/",
+      name: "Sign Out",
+      href: "#",
       isMobile: isMobile,
       handler: ()=>signOut(),
     }
@@ -35,34 +35,26 @@ export default function AuthButton(){
       <div className="hidden md:block">
         <div className="ml-4 flex items-center md:ml-6">
         { !session ?
-          <span 
-            className="cursor-pointer text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          <div 
+            className="relative flex items-center cursor-pointer "
             onClick={()=>signIn()}
           >
             Sign In
-          </span>
+          </div>
           :
           <Menu as="div" className="relative">
             <Menu.Button className="relative flex items-center cursor-pointer ">
-              <span className="text-gray-400 mx-2">{session.user.name}</span>
+              <div className="text-gray-400 mx-2">{session.user.name}</div>
               <Image src={session.user.image} width={40} height={40} alt="no Photo"/>
             </Menu.Button>
             <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <Menu.Item>
-                {({active})=>(
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" >Your Profile</a>
-                )}  
-              </Menu.Item>
-              <Menu.Item>
-                {({active})=>(
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" >Settings</a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({active})=>(
-                  <a href="/" onClick={()=>signOut} className="block px-4 py-2 text-sm text-gray-700">Sign out</a>
-                )}
-              </Menu.Item>
+              { 
+                menuItems.map((item, i)=>
+                  <Menu.Item>
+                    {({active})=><a href={item.href} className={item.isMobile(false)} onClick={item.handler}>{item.name}</a>}  
+                  </Menu.Item>
+                )
+              }
             </Menu.Items> 
           </Menu>
         } 
@@ -70,24 +62,31 @@ export default function AuthButton(){
       </div>
       
       <div className="md:hidden">
-        <div className="border-t border-gray-700 pb-3 pt-4">
-          <div className="flex items-center px-5">
-            <div className="flex-shrink-0">
-            </div>
-            <div className="ml-3">
+        {
+          !session ? 
+          <span 
+            className="cursor-pointer text-gray-300 hover:text-white -md px-3 py-2 text-sm font-medium"
+            onClick={()=>signIn()}
+          >
+            Sign In
+          </span>
+          :
+          <Menu as="div"className="border-t border-gray-700 pb-3 pt-4">
+            <Menu.Button className="flex flex-col px-5">
               <div className="text-base font-medium leading-none text-white">{session?.user.name}</div>
               <div className="text-sm font-medium leading-none text-gray-400">{session?.user.email}</div>
-            </div>
-          </div>
-          { session ?
-            <div className="mt-3 space-y-1 px-2">
-              <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
-              <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-              <span onClick={()=>signOut()} className="cursor-pointer block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</span>
-            </div>
-            : "" 
-          }
-      </div>
+            </Menu.Button>
+            <Menu.Items className="mt-3 space-y-1 px-2">
+            { 
+              menuItems.map((item, i)=>
+                <Menu.Item>
+                  {({active})=><a href={item.href} className={item.isMobile(true)} onClick={item.handler}>{item.name}</a>}  
+                </Menu.Item>
+              )
+            }
+            </Menu.Items> 
+          </Menu>
+        }
       </div>    
     </div>
   )
