c6daee37da52a9daa722288bd76f7f2bd921bc4c
inzilion <inzilion@gmail.com>
2023-10-12 13:57:05 +0900

    add name

--- src/components/option.js
+++ src/components/option.js
@@ -1,7 +1,7 @@
 export default function Option({item}){
   return(
     <div>
-      <input type="checkbox" defaultChecked={item.isTrue}/>{item.content}
+      <input name="option" type="checkbox" defaultChecked={item.isTrue}/>{item.content}
     </div>
   )
 }
--- src/components/selection.js
+++ src/components/selection.js
@@ -1,13 +1,15 @@
 export default function Selection({id, options, handler}){
   return(
-    <select 
-      id = {id}
-      className="h-full ring-1 rounded-md border-1 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-      onChange={(e)=>handler(e)}
-    >
-    {
-      options.map((e, i)=><option key={i}>{e}</option>)
-    }
-    </select>
+    <div>
+      <select 
+        id = {id}
+        className="h-full ring-1 rounded-md border-1 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+        onChange={(e)=>handler(e)}
+      >
+      {
+        options.map((e, i)=><option key={i}>{e}</option>)
+      }
+      </select>
+    </div>
   )
 }
