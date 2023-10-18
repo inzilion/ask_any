0da00fec0beda3a0d55ce1a33cf2588b9a9fd175
inzilion <inzilion@gmail.com>
2023-10-18 11:00:22 +0900

    리스트에 정답률 표시

--- src/app/problem/list/page.js
+++ src/app/problem/list/page.js
@@ -4,15 +4,19 @@ export default async function List(){
   const list = await db.collection("PROBLEMS").find().toArray();
   return(
     <>
-      <ul className="divide-y divide-gray-100 pl-3">
+      <ul className="divide-y divide-gray-100 p-3">
         {list.map((item, i) => (
           <li key={i} className="flex justify-between gap-x-6 py-5">
-            <div className="flex min-w-0 gap-x-4">
+            <div className="flex min-w-0 gap-x-4 pl-3">
               <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={item.author?.image} alt="" />
               <div className="min-w-0 flex-auto">
                 <a href={`/problem/${item._id.toString()}`} className="text-sm font-semibold leading-6 text-gray-900">{item.title}</a>
                 <p className="mt-1 truncate text-xs leading-5 text-gray-500">출제자: {item.author.name}</p>
               </div>
+            </div>
+            <div className='pr-3'>
+              <div className='text-xs'>정답률</div>
+              <div className='text-xs'>{(item.countRight/item.countTry*100).toFixed(1)}%</div>
             </div>
           </li>
         ))}
