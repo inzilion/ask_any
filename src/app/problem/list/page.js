1b8d0ce9fee49ec4a90ede69809be456ba55c84f
inzilion <inzilion@gmail.com>
2023-10-24 09:54:54 +0900

    list 인터페이스 수정

--- src/app/problem/list/page.js
+++ src/app/problem/list/page.js
@@ -18,16 +18,16 @@ export default async function List(){
             <div className="flex min-w-0 gap-x-4 pl-3">
               {/* <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={item.author?.image} alt="" /> */}
               <div>
-                { userData ? (userData.problems[item._id] === undefined ? <div className="w-3"></div>
+                { userData ? (userData.problems[item._id] === undefined ? <div className="w-4"></div>
                   : userData.problems[item._id]?.isSolved ? <FontAwesomeIcon style={{color: "green"}} icon={faCheck}/> 
                   : <FontAwesomeIcon style={{color: "red"}} icon={faXmark}/>) : ""}
               </div>
-              <div className="min-w-0 flex-auto">
+              <div className="flex-col">
                 <a href={`/problem/${item._id.toString()}`} className="text-sm font-semibold leading-6 text-gray-900">{item.title}</a>
                 <p className="mt-1 truncate text-xs leading-5 text-gray-500">출제자: {item.author.name}</p>
               </div>
             </div>
-            <div className='pr-3'>
+            <div className='flex justify-items-end flex-col w-9'>
               <div className='text-xs'>정답률</div>
               <div className='text-xs'>{(item.countRight/item.countTry*100).toFixed(1)}%</div>
             </div>
