69b65f2cd88d50a2c9042918dc7ff143a1ff4d3a
inzilion <inzilion@gmail.com>
2023-11-13 11:50:59 +0900

    대회 userAnswers 오류 처리 중

--- src/components/contest/contestMain.js
+++ src/components/contest/contestMain.js
@@ -29,17 +29,31 @@ export default function ContestMain({contestID}){
     console.log(userAnswers);
   },[userAnswers]);
 
+  const setUserAnswersHandler = (e, i) => {
+    if(e.target.id === "선택형")
+      return setUserAnswers(answers => {answers[currentPtr] = e.target.value; return answers});
+    setUserAnswers(answers => {
+      answers[currentPtr] = answers[currentPtr].map((op, j) => {
+        op.isTrue = i==j ? e.target.checked : op.isTrue;
+        return op;
+      });
+      return answers;
+    })
+  }
+
   return(
     <>
       <div>
         <div className='text-xl text-center'>{contestID}</div>
         <div>
           {
-            contest._id != undefined
+            userAnswers?.length > 0
             ? <ShowProblem 
                 problemData={contest.problems[currentPtr]} 
                 userData={userAnswers[currentPtr]}
+                handler={setUserAnswersHandler}
               />
+              
             : ""
           }
         </div>
