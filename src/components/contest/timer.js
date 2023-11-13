0d8cd1a7aa14ebcee732380d7d9dde6bfe11b6ce
inzilion <inzilion@gmail.com>
2023-11-13 10:02:44 +0900

    대회 문제만 검색하여 데이터 전달

---
+++ src/components/contest/timer.js
@@ -0,0 +1,7 @@
+export default function Timer({limit}){
+  return(
+    <>
+      <div className='bg-lime-500'>Timer 컴포넌트 놓을 자리</div>
+    </>
+  )
+}
--- src/pages/api/contest/get.js
+++ src/pages/api/contest/get.js
@@ -1,12 +1,25 @@
 import client from '@/util/database';
 import { ObjectId } from 'mongodb';
+import fs from "fs";
 
 export default async function handler(req, res){
   const contestID = JSON.parse(req.body).contestID;
-  console.log(contestID);
   const db = client.db("ASK_ANY");
   const contest = await db.collection('CONTESTS').findOne({_id: new ObjectId(contestID)})
-  console.log(contest.title);
 
+  let problemList = await db.collection('PROBLEMS').find({_id: { $in: contest.problems.map(p=> new ObjectId(p))}}).toArray(); 
+  
+  problemList = problemList.map(p=>{
+    if(p.image)
+      try{
+        p.image = fs.readFileSync(p.image, 'utf-8');
+      } catch {
+        p.image = '';
+      }
+    return p;
+  }) 
+  
+  contest.problems = problemList;
+  
   return res.json(JSON.stringify(contest));
 }
