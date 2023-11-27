cc2795b3c5db4278783ad81ac8a361f835dc8a10
inzilion <inzilion@gmail.com>
2023-11-27 16:14:27 +0900

    평가정오표 출력완료

--- src/app/admin/eval/page.js
+++ src/app/admin/eval/page.js
@@ -6,30 +6,34 @@ export default async function Eval({searchParams}){
   const db = client.db("ASK_ANY");
   const contest = await db.collection("CONTESTS").findOne({_id: new ObjectId(contestID)})
   const problemList = await db.collection('PROBLEMS').find({_id: { $in: contest.problems.map(p=> new ObjectId(p))}}).toArray(); 
-  console.log(contest.participants)
+
+  const evalTable = Array.from(Array(contest.participants.length+1), ()=>new Array());
+  evalTable[0].push("아이디");
+  problemList.map((p, i) => evalTable[0].push(`${i+1}번`));
+
+  contest.participants.map((part, i)=>{
+    evalTable[i+1].push(part.email);
+    problemList.map((pro, j)=>{
+      evalTable[i+1].push(
+        pro.options.length > 0 && contest.participants[i].answers.length > 0
+        ? pro.options.map((op, k)=>op.isTrue == contest.participants[i].answers[j].options[k].isTrue).filter(e=>e).length==pro.options.length ? true: false
+        : pro.answer == contest.participants[i].answers[j]?.answer ? true : false
+      )
+    })
+  })
+  console.log(evalTable);
+  
   return(
     <>
-      {
-        <table className="table-auto w-full mt-2">
-        <thead>
-          <tr className="flex justify-between justify-center p-2 bg-green-500 text-white">
-            <th className="">이름</th>
-            {
-              problemList.map((p, i)=><th className="text-center">{i+1}</th>)
-            }
-          </tr>
-        </thead>
-        <tbody>
-          {
-            contest.participants.map((part, i)=>
-              <tr key={i}>
-                <td>{part.email.slice(0,6)}</td>
-              </tr>
-            )
-          }
+      <table className="table-auto w-full mt-2">
+        <tbody className="divide-y divide-gray-300">
+        {
+          evalTable.map((row, i)=>
+            <tr key={i}>{row.map((el, j)=><td key={j}>{typeof(el)=='boolean'? el ? "⭕":"❌" : el}</td>)}</tr>
+          )
+        }
         </tbody>
-        </table>         
-      }
+      </table>         
     </>
   )
 }
--- src/components/admin/contestList.js
+++ src/components/admin/contestList.js
@@ -3,9 +3,9 @@ export default function ContestList({list}){
     <>
       <table className="table-auto w-full mt-2">
         <thead>
-          <tr className="flex justify-between justify-center p-2 bg-blue-500 text-white">
+          <tr className="flex justify-between justify-center p-1 bg-yellow-500">
             <th className="w-1/12">구분</th>
-            <th className="w-6/12">대회명</th>
+            <th className="w-6/12">평 가 명</th>
             <th className="w-1/12">문제수</th>
             <th className="w-2/12">제한시간</th>
             <th className="w-1/12">상태</th>
@@ -18,7 +18,7 @@ export default function ContestList({list}){
             return( 
               <tr key={i} className="flex justify-between justify-center p-1 border-b border-blue-300" >
                 <td className="grid justify-items-center w-1/12">{contest.contestType}</td>
-                <td className="grid justify-items-center w-6/12">{isEndContest ? contest.title : <a href={`/contest/${contest._id}`}>{contest.title}</a>}</td>
+                <td className="grid justify-items-center w-6/12"><a href={`/contest/${contest._id}`}>{contest.title}</a></td>
                 <td className="grid justify-items-center w-1/12">{contest.numOfProblems}</td>
                 <td className="grid justify-items-center w-2/12">{contest.period}분</td>
                 <td className="grid justify-items-center w-1/12">{isEndContest ? <p className='text-rose-500'>종료</p> : <p className='text-green-500'>진행중</p>}</td>
